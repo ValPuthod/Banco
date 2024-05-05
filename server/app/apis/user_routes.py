21827a6059ec29c5d153a249fe0f81d94f5bf9ab
Hammad Saeed <hammad.saeed776@gmail.com>
2024-05-05 15:21:55 -0700

    Add comments in the APIs and helper methods

--- server/app/apis/user_routes.py
+++ server/app/apis/user_routes.py
@@ -13,15 +13,17 @@ router = APIRouter()
 
 
 @router.post("/register", response_model=UserToken, status_code=status.HTTP_201_CREATED)
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    # Check if user already exists by email to prevent duplicates
     is_exist = db.query(User).filter(User.email == user.email).first()
     if is_exist:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="User already exist with this email!",
         )
-
+    # Create a new user in the database
     user = create_new_user(user=user, db=db)
+    # Generate access token for the new user
     access_token = create_access_token(
         data={"sub": user.email}
     )
@@ -29,14 +31,15 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
 
 
 @router.post("/login", response_model=UserToken)
-def login_for_access_token(user_data: UserLogin, db: Session = Depends(get_db)):
+def login_user(user_data: UserLogin, db: Session = Depends(get_db)):
+    # Verify if user exists by email
     is_exist = db.query(User).filter(User.email == user_data.email).first()
     if not is_exist:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="No user exist with this email!",
         )
-
+    # Authenticate user credentials
     user = authenticate_user_credentials(
         user_data.email, user_data.password, db)
     if not user:
@@ -44,42 +47,41 @@ def login_for_access_token(user_data: UserLogin, db: Session = Depends(get_db)):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect email or password!",
         )
+    # Generate access token for the authenticated user
     access_token = create_access_token(
         data={"sub": user.email}
     )
-
     return {"token": {"access_token": access_token, "token_type": "bearer"}, "user": user.to_dict()}
+
+
+@router.get("/users", response_model=List[ShowUser], status_code=status.HTTP_200_OK)
+def get_all_users(current_user: ShowUser = Depends(authenticate_user_token), db: Session = Depends(get_db)):
+    # Fetch all users from the database except the current user
+    users = db.query(User).filter(
+        User.email != current_user.email).order_by(User.id).all()
+    return [user.to_dict() for user in users]
 
 
 @router.get("/me", response_model=ShowUser, status_code=status.HTTP_200_OK)
 def get_current_user(current_user: ShowUser = Depends(authenticate_user_token)):
-
+    # Return the profile of the current authenticated user
     return current_user.to_dict()
 
 
 @router.put("/profile", response_model=ShowUser, status_code=status.HTTP_200_OK)
 def update_user_profile(user_payload: UserProfileUpdate, current_user: ShowUser = Depends(authenticate_user_token), db: Session = Depends(get_db)):
+    # Update the user's profile information
     user = update_user(current_user, user_payload, db)
-
     return user
 
 
 @router.put("/password", status_code=status.HTTP_200_OK)
 def update_user_password(password_payload: PasswordUpdate, current_user: ShowUser = Depends(authenticate_user_token), db: Session = Depends(get_db)):
+    # Update user's password
     is_updated = update_password(current_user.email, password_payload, db)
-
     if not is_updated:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Unable to updated password!",
         )
-
     return {"detail": "Password updated successfully!"}
-
-
-@router.get("/users", response_model=List[ShowUser], status_code=status.HTTP_200_OK)
-def get_all_users(current_user: ShowUser = Depends(authenticate_user_token), db: Session = Depends(get_db)):
-    users = db.query(User).filter(
-        User.email != current_user.email).order_by(User.id).all()
-
-    return [user.to_dict() for user in users]
--- server/app/utils/user.py
+++ server/app/utils/user.py
@@ -1,48 +1,69 @@
 from typing import Optional
-from datetime import datetime
-from datetime import timedelta
 from jose import JWTError, jwt
 from app.db.session import get_db
 from sqlalchemy.orm import Session
-from app.core.config import settings
 from app.core.hashing import Hasher
+from app.core.config import settings
+from datetime import datetime, timedelta
 from app.db.models.user import get_user_by_email
 from fastapi import Depends, HTTPException, Request, status
 
 
 def authenticate_user_token(request: Request, db: Session = Depends(get_db)):
+    """
+    Authenticate user based on the JWT token passed in the request headers.
+    Raises HTTPException if token is invalid or user does not exist.
+    """
     try:
+        # Decode the JWT to extract user email
         email = decode_access_token(
             request.headers.get('Authorization', ''))
+        # Retrieve user by email from the database
         user = get_user_by_email(email=email, db=db)
         if not user:
+            # User not found or token was invalid
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Invalid access token!",
             )
         return user
     except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid access token!")
+        # Error in decoding the JWT, likely due to manipulation or expiration
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid access token!")
 
 
 def authenticate_user_credentials(email: str, password: str, db: Session):
+    """
+    Authenticate user credentials by verifying the email and password.
+    Returns the user object if authentication is successful, False otherwise.
+    """
+    # Retrieve user by email from the database
     user = get_user_by_email(email=email, db=db)
     if not user:
+        # No user found with the given email
         return False
     if not Hasher.verify_password(password, user.password):
+        # Password does not match
         return False
     return user
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    """
+    Create a JWT access token that expires in a given timedelta or the default set in settings.
+    """
     to_encode = data.copy()
     if expires_delta:
+        # Set expiration with provided delta
         expire = datetime.utcnow() + expires_delta
     else:
+        # Default expiration time from settings
         expire = datetime.utcnow() + timedelta(
             minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
         )
     to_encode.update({"exp": expire})
+    # Encode the JWT token
     encoded_jwt = jwt.encode(
         to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
     )
@@ -50,6 +71,10 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
 
 
 def decode_access_token(access_token: str):
+    """
+    Decode the JWT access token to extract the subject (sub) which contains the user's email.
+    """
+    # Decode the JWT
     decoded_jwt = jwt.decode(access_token,
                              settings.SECRET_KEY, settings.ALGORITHM)
     return decoded_jwt['sub']
